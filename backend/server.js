606009d5433a6edb3e1bf1376e512131be187757
Lakshya Pappu <lakshyapappu07@gmail.com>
2026-04-21 20:35:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,12 @@ const PORT = process.env.PORT || 5000;
 const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/student_complaint_system";
 
-app.use(cors());
+app.use(cors({
+  origin: "https://student-complaint-system-49er.onrender.com",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api", authRoutes);
